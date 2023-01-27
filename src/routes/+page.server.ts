ff992925d83e38b14bbf1fd9fcfce79110afa57c
Ian Flanagan <ian@quanta-inc.com>
2023-01-27 13:39:49 -0500

    feat: logic for checking associations

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,5 @@
+import type { Item, Client, Location, Call, Device, Contract } from '../lib/selected';
+
 let result = {
     clients: [{
         ID: "1",
@@ -41,6 +43,63 @@ let result = {
     }]
 }
 
+
+
+function getAss(s: string): string[] {
+    let a = [];
+
+    //only "upward" associations
+    let sa = []; //SELECT * FROM wherever WHERE whatever = whatever
+    
+    if (sa.length > 0) { //if there are associations...
+        sa.forEach((item) => { //...recursively find associations for those
+            a.push(...getAss(item));
+        });
+    } else { //else, if this is the end of the line
+        a.push(s);
+    }
+    return a;
+}
+
+
+//unused for now, this would be used for the multi-select
+//version if that ever gets made
+
+function getAssociations(selected: string[]) {
+    let associations: string[] = [];
+
+    //for each selected device
+    selected.filter((item) => item.type == "Device").forEach((device) => {
+        associations.push(...getAss(device));
+    });
+
+    //for each selected location
+    selected.filter((item) => item.type == "Location").forEach((location) => {
+        associations.push(...getAss(location));
+    });
+
+    //for each selected call
+    selected.filter((item) => item.type == "Call").forEach((call) => {
+        associations.push(...getAss(call));
+    });
+
+    //for each selected contract
+    selected.filter((item) => item.type == "Contract").forEach((contract) => {
+        associations.push(...getAss(contract));
+    });
+
+    //for each selected client
+    selected.filter((item) => item.type == "Client").forEach((client) => {
+        associations.push(...getAss(client));
+    });
+
+    return associations;
+}
+
+    //for each selected contract
+
+    //for each selected client
+}
 
 export const load = () => {
     return result;
