940bd00d983c520e288260ac492a133dc38b566d
Trevor Allen <racierjet@hotmail.com>
2023-01-31 12:27:16 -0500

    Revert "Frontend"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,85 +1,16 @@
 <script lang="ts">
-    import List from "../lib/list/List.svelte";
-    //import ListItem from './lib/list/ListItem.svelte';
-    import Info from "../lib/list/Info.svelte";
-    import { Card } from "flowbite-svelte";
-    import { writable } from "svelte/store";
-    import  {Selected} from "../lib/selected";
-    import type { Item } from "../lib/selected"
-    import type { Client, Location, Device, ServiceCall, Contract } from '@prisma/client';
+	import type { Client, Location, Device, ServiceCall, Contract } from '@prisma/client';
 
+	export let data: {
+		cilents: Client[];
+		locations: Location[];
+		devices: Device[];
+		calls: ServiceCall[];
+		contracts: Contract[];
+	};
+</script>
 
-    export let data: {
-		  cilents: Client[];
-		  locations: Location[];
-		  devices: Device[];
-		  calls: ServiceCall[];
-		  contracts: Contract[];
-	  };
-  
-  </script>
-  
-  <main class="flex">
+{data.cilents}
 
-    <div class="bottom grow gap-1">
-      <div>
-        <List items={data?.clients} />
-        <List name="Contracts" columns={[
-          ["Name", true],
-
-          ["Months left", false]
-        ]}/>
-      </div>
-      <div>
-        <List
-        name="Locations"
-        columns={[
-          ["Name", true],
-          ["Address", true],
-        ]}
-        items={data?.locations}
-      />
-      <List
-        name="Calls"
-        columns={[
-          ["CallID", true],
-          ["Date", true],
-          ["3rd thing", false],
-        ]}
-        items={data?.calls}
-      />
-      </div>
-  
-      
-      <List
-        name="Devices"
-        columns={[
-          ["Model#", true],
-          ["Serial#", true],
-        ]}
-        items={data?.devices}
-      />
-      
-    </div>
-    <div class="top gap-1">
-      <Info  />
-      <!-- <Value  /> -->
-    </div>
-  </main>
-  
-  <style>
-    .top {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
-      gap: 15px 0px;
-    }
-    .bottom {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
-      flex-wrap: wrap;
-      gap: 15px 0px;
-    }
-
-  </style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
